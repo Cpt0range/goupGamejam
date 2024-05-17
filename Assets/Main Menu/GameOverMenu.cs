3ffc5a67be48f8e63b1c2949f6322c08a5059464
Markus <kohfinkm@gmail.com>
2024-05-17 10:00:24 +0200

    Menu teils verbessert/verschlechtert

---
+++ Assets/Main Menu/GameOverMenu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverMenu : MonoBehaviour
+{
+    public void Tryagain()
+    {
+        SceneManager.LoadScene(6);
+    }
+}
